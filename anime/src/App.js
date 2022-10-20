825aaa73acb05d07ccb3d312d3af5e36ff448463
lulusadowsky <sadowska.luiza@gmail.com>
2022-10-20 13:38:02 +0200

    structure, components, css

---
+++ anime/src/App.js
@@ -0,0 +1,21 @@
+import {useState, useEffect} from "react";
+import Header from './components/Header'
+import Sidebar from "./components/Sidebar";
+
+function App() {
+    const [animeList, setAnimeList] = useState([]);
+    const [topAnime, setTopAnime] = useState([]);
+    const [search, setSearch] = useState("");
+
+  return (
+    <div className="App">
+      <Header />
+        <div className="content-wrap">
+            <Sidebar
+                topAnime={topAnime} />
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ anime/src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Sidebar(){
+    return (
+        <aside>
+            <nav>
+                <h3>Top Anime</h3>
+                <a
+                    href="#"
+                    target="_blank"
+                    rel="noreferrer">
+                    Attack on Titan this is my top anime
+                 </a>
+            </nav>
+        </aside>
+    )
+}
+export default Sidebar
